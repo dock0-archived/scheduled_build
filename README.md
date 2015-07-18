2a5ba3ec84f2cad54117546838db35444a7aab56
Les Aker <me@lesaker.org>
2015-07-17 22:43:44 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+scheduled\_build
+=======
+
+[![MIT Licensed](http://img.shields.io/badge/license-MIT-green.svg)](https://tldrlegal.com/license/mit-license)
+
+Lightweight scripts to trigger builds of my Automated Builds on Docker Hub
+
+## Usage
+
+## License
+
+This code is released under the MIT License. See the bundled LICENSE file for details.
+
---
+++ bin/trigger_build.rb
@@ -0,0 +1,21 @@
+#!/usr/bin/env ruby
+
+require 'curb'
+require 'json'
+
+LIST = ARGV.empty? ? (ENV['TRIGGER_TOKENS'] || '').split('|') : ARGV
+fail('No arguments or env var provided') if LIST.empty?
+
+URL = 'https://registry.hub.docker.com/u/%s/trigger/%s/'
+
+CLIENT = Curl::Easy.new do |c|
+  c.headers['Content-Type'] = 'application/json'
+end
+
+PAYLOAD = {'build' => true}
+
+LIST.map { |x| x.split(':') }.each do |repo, token|
+  repo_url = URL % [repo, token]
+  CLIENT.url = repo_url
+  CLIENT.http_post(PAYLOAD)
+end
