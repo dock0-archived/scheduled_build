af445fde5858954c5bc19b2117d4b72196b9e289
Les Aker <me@lesaker.org>
2016-09-06 23:00:03 -0400

    set up rubocop tests

--- bin/trigger_build.rb
+++ bin/trigger_build.rb
@@ -4,19 +4,19 @@ require 'curb'
 require 'json'
 
 LIST = ARGV.empty? ? (ENV['TRIGGER_TOKENS'] || '').split('|') : ARGV
-fail('No arguments or env var provided') if LIST.empty?
+raise('No arguments or env var provided') if LIST.empty?
 
-URL = 'https://registry.hub.docker.com/u/%s/trigger/%s/'
+URL = 'https://registry.hub.docker.com/u/%s/trigger/%s/'.freeze
 
 CLIENT = Curl::Easy.new do |c|
   c.headers['Content-Type'] = 'application/json'
 end
 
-PAYLOAD = {'build' => true}
+PAYLOAD = { 'build' => true }.freeze
 
 LIST.map { |x| x.split(':') }.each do |repo, token|
   puts "Triggering build for #{repo}"
-  repo_url = URL % [repo, token]
+  repo_url = format URL repo, token
   CLIENT.url = repo_url
   CLIENT.http_post(PAYLOAD)
 end
